170097cec44f146641427145cdd8db40005decf9
grkon <yo.for.user@gmail.com>
2022-09-24 08:19:27 +0900

    make base

---
+++ src/collection.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "header/cube.h"
---
+++ src/header/cube.h
@@ -0,0 +1,17 @@
+/*
+cube.h
+
+Definitions of the game board of 3 dimensional 4 in a row.
+*/
+
+#pragma once
+
+namespace blinear
+{
+    /*
+    The game board of 3 dimensional 4 in a row.
+    */
+    class Cube
+    {
+    };
+}
